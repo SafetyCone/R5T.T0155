140e5bb15585a742483e11fb2be83c162493e80e
davidcoats <david@getsafetycone.com>
2022-12-11 14:56:38 -0800

    Initial commit.

---
+++ source/R5T.T0155.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0155.Construction
+Construction console project for the R5T.T0155 library.
---
+++ source/R5T.T0155/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0155
+{
+	/// <summary>
+	/// Delegate marker attributes and interfaces.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
