84e69b1f30abf28915c1ad61080fd93ecdac9263
davidcoats <david@getsafetycone.com>
2023-03-23 02:29:37 -0700

    Interim changes.

--- source/R5T.T0155/Code/Interfaces/IDelegateMarker.cs
+++ source/R5T.T0155/Code/Interfaces/IDelegateMarker.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿/*
+ * Note! Delegates cannot implement interfaces, so there is not need for this interface.
+ * 
+ * The file is kept here to match the general marker attribute pattern.
+ */
 
 
-namespace R5T.T0155
-{
-    /// <summary>
-    /// Marker interface for delegates (well... delegates that should be surveyed).
-    /// Allows methods-for, and extension methods-on, all types that are marked delegates.
-    /// </summary>
-    public interface IDelegateMarker
-    {
-    }
-}
+//using System;
+
+
+//namespace R5T.T0155
+//{
+//    /// <summary>
+//    /// Marker interface for delegates (well... delegates that should be surveyed).
+//    /// Allows methods-for, and extension methods-on, all types that are marked delegates.
+//    /// </summary>
+//    public interface IDelegateMarker
+//    {
+//    }
+//}
--- source/R5T.T0155/Code/Interfaces/IDraftDelegateMarker.cs
+++ source/R5T.T0155/Code/Interfaces/IDraftDelegateMarker.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿/*
+ * Note! Delegates cannot implement interfaces, so there is not need for this interface.
+ * 
+ * The file is kept here to match the general marker attribute pattern.
+ */
 
 
-namespace R5T.T0155
-{
-    /// <summary>
-    /// Marker interface for all draft delegates.
-    /// Allows methods-for, and extension methods-on, all types that are marked draft delegates.
-    /// </summary>
-    public interface IDraftDelegateMarker
-    {
-    }
-}
+//using System;
+
+
+//namespace R5T.T0155
+//{
+//    /// <summary>
+//    /// Marker interface for all draft delegates.
+//    /// Allows methods-for, and extension methods-on, all types that are marked draft delegates.
+//    /// </summary>
+//    public interface IDraftDelegateMarker
+//    {
+//    }
+//}
